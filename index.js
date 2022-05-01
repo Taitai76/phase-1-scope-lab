3864c978a621fb80abb4d88e1f954b8ca811e031
Mohammed Taitai <mohammedtaitai@outlook.com>
2022-05-01 16:24:20 -0400

    commit tree 8c5421923442d2dfe9862ed38dd49fd2adff636a

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-// Write your solution in this file!
+customerName = "bob";
+const leastFavoriteCustomer = "bob";
+function upperCaseCustomerName(){
+    customerName="BOB"
+    return customerName;
+
+}
+
+function setBestCustomer(){
+    bestCustomer="not bob";
+    return bestCustomer;
+}
+
+function overwriteBestCustomer(){
+    bestCustomer="maybe bob";
+    return bestCustomer;
+}
+
+function changeLeastFavoriteCustomer(){
+    leastFavoriteCustomer= "Not bob";
+    return x;
+
+}
